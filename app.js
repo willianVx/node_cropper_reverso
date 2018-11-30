dc82ab5ef4c24685b4dd9daedf5a435bbcd98bcb
willianVx <willianvoliveiravx@gmail.com>
2018-11-30 17:43:05 -0200

    Arrumando app js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.send('Hello World!');
-}).listen(8080, 'instaarts.com');
+}).listen(8080, 'https://instaarts.com/');
 console.log('Server running at http://67.222.18.223:8080/');
 /*
 var app = require ('./config/server');
