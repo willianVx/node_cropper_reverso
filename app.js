0cd452528719fba9d6098efa56655a5d6d9e149e
willianVx <willianvoliveiravx@gmail.com>
2018-11-30 17:25:59 -0200

    Arrumando app js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, 'http://67.222.18.223');
+  res.send('Hello World');
+}).listen(8080);
 console.log('Server running at http://67.222.18.223:8080/');
 /*
 var app = require ('./config/server');
