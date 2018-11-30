825f32779c18c2d51a658debbad8078b023a7c16
willianVx <willianvoliveiravx@gmail.com>
2018-11-30 17:20:48 -0200

    teste para rodar no servidor

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, 'http://67.222.18.223');
+console.log('Server running at http://67.222.18.223:8080/');
+/*
 var app = require ('./config/server');
 
 app.listen(8080, function(){
     console.log('Servidor online!');
 })
+*/
