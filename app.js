18b0e86c05033b3bef771cceae4bd14fe269cea4
willianVx <willianvoliveiravx@gmail.com>
2018-12-03 15:11:03 -0200

    testes no servidor - 9

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ console.log('Server running at http://arteref.com');
 */
 var app = require ('./config/server');
 
-var port = process.env.PORT || 8080;
+var port = 60000;
 var server_ip_address = "67.222.18.223";
 
-app.listen(port, server_ip_address, function(){
-    console.log('Servidor online! ' + port + ", IP " + server_ip_address);
+app.listen(port, function(){
+    console.log('Servidor online! ' + port);
 })
