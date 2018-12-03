7b1afe718a70fa10b8f0e2e7ca0f2192da10e720
willianVx <willianvoliveiravx@gmail.com>
2018-12-03 16:59:52 -0200

    testes no servidor - 11

--- app.js
+++ app.js
@@ -6,26 +6,13 @@ http.createServer(function (req, res) {
 }).listen(8080);
 console.log('Server running at http://arteref.com');
 
+*/
 var app = require ('./config/server');
 
-var port = 60000;
+var port = process.env.PORT || 8080;
+app.set(process.env.PORT || 8080)
 var server_ip_address = "67.222.18.223";
 
-app.listen(port, function(){
-    console.log('Servidor online! ' + port);
+app.listen(app.get('port'), function(){  
+    console.log('Servidor online! ' + app.get('port'));
 })
-*/
-const http = require('http')
-const port = 3000
-const ip = 'instaarts.com'
-
-const server = http.createServer((req, res) => {
-  console.log('Recebendo uma request!')
-  res.end('<h1>Aqui fica o que vamos enviar para o navegador como resposta!</h1>')
-})
-
-server.listen(port, ip, () => {
-  console.log(`Servidor rodando em http://${ip}:${port}`)
-  console.log('Para derrubar o servidor: ctrl + c');
-})
-
