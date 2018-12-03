53003c0f2c3cf8497f635030639703778f489476
willianVx <willianvoliveiravx@gmail.com>
2018-12-03 14:59:56 -0200

    testes no servidor - 7

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ var app = require ('./config/server');
 var port = process.env.PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-app.listen(port, server_ip_address, function(){
+app.listen(port, "67.222.18.223", function(){
     console.log('Servidor online! ' + port + ", IP " + server_ip_address);
 })
