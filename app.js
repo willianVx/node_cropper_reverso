c1847460087cbe0f20357087e005e85f27383e4d
willianVx <willianvoliveiravx@gmail.com>
2018-12-04 18:36:39 -0200

    testes no servidor - 17

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/*
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
@@ -7,20 +7,19 @@ var server = http.createServer(function (req, res) {
 });
 server.listen(80, '67.222.18.224');
 console.log('Server running at http://arteref.com');
+*/
 
 
 
-/*
 var app = require ('./config/server');
 
 //var port = process.env.PORT || 8080;
 //app.set(process.env.PORT || 8080);
 
-var port = 80;
+var port = 8080;
 
 var server_ip_address = "67.222.18.224";
 
-app.listen(port, '67.222.18.224', function(){  
+app.listen(port, function(){  
     console.log('Servidor online! ' + port);
 })
-*/
